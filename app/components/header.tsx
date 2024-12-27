c0ae5baa7c8bce92278931d9082fdbe8b12e1880
me <slavaveto@gmail.com>
2024-12-26 23:44:58 -0300

    перенес меню в отдальный файл

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,4 +1,4 @@
-//"use client";
+"use client";
 
 import {
     Navbar,
@@ -9,18 +9,30 @@ import {
     NavbarMenu,
     NavbarMenuItem
 } from "@nextui-org/navbar";
-import {Button} from '@nextui-org/button';
+
+import MobileMenu from "@/app/components/mobileMenu";
 
 import {HiMiniBars3} from 'react-icons/hi2';
 
 function Header(props: any) {
     return (
+
         <Navbar shouldHideOnScroll className="-hidden -md:block mb-5 bg-blue-100 border-b -border-divider"
                 maxWidth="full">
 
             <div className="flex container mx-auto px-0 sm:px-3 items-center justify-between">
-                <div>
-                    <HiMiniBars3 size={30} className="-ml-1 md:hidden"/>
+                <div className="flex items-center">
+                    <button className="-ml-1 md:hidden"
+                            //onClick={onOpen}
+                            style={{
+                                background: 'none',
+                                border: 'none',
+                                cursor: 'pointer',
+                            }}
+                    >
+                        <HiMiniBars3 size={30} className="-ml-1 md:hidden"/>
+                    </button>
+
                 </div>
                 <div className="text-right">
                     <p className="font-medium text-lg leading-4 pb-1">Вячеслав Вето</p>
