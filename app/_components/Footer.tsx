20460e62aaa37b63d758c12e2b1f4f71062bd024
me <slavaveto@gmail.com>
2024-12-29 09:41:02 -0300

    добавил работу спиннера первичной загрузки

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -6,8 +6,8 @@ import LangToggle from "@/app/_components/LangToggle";
 
 function Footer() {
     return (
-        <footer className="my_footer_bg flex h-[60px] mt-5 items-center">
-            <div className="container flex mx-auto px-3 justify-between items-center">
+        <footer className="my_footer_bg flex h-[60px] mt-5 items-center ">
+            <div className="container flex mx-auto px-3 justify-between items-center max-w-screen-md">
                 <div>
                     <p className="font-medium text-lg leading-4 pb-1">Вячеслав Вето</p>
                     <p className="leading-4 text-sm italic">персональный сайт психолога</p>
